67d8d21fe7f0e420caf0364ed39d265af0cb32f0
fnobi <shin@fnobi.com>
2012-09-26 20:24:13 +0900

    "};"もありでした。結局"}("以外は許すことに。

--- init.el
+++ init.el
@@ -138,7 +138,7 @@
      ("\t" 0 my-face-b-2 append)
      ("　" 0 my-face-u-1 append)
      ("[^ ]{" 0 my-face-u-1 append)
-     ("}[^ ]" 0 my-face-u-1 append)
+     ("}[(]" 0 my-face-u-1 append)
      (",[^ ]" 0 my-face-u-1 append)
      ("[ \t]+$" 0 my-face-b-1 append)
      (" [\r]*\n" 0 my-face-r-1 append)
