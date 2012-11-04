d3d90119ca6f90e32f4516025b9daa3b5f95ecf7
fnobi <shin@fnobi.com>
2012-11-05 00:56:19 +0900

    結局、multi-termを導入することに。

--- init.el
+++ init.el
@@ -143,16 +143,36 @@
 ;; windows
 ;;(load "init-windows")
 
-;; multi-term
-;;(require 'multi-term)
-;;(setq multi-term-program "/bin/bash")
-
 ;; howm
 ;; (load "init-howm")
 
 ;; mail
 (load "init-mail")
 
+
+;;============================================================================;;
+;; terminal                                                                   ;;
+;;============================================================================;;
+;; より下に記述した物が PATH の先頭に追加されます
+(dolist (dir (list
+              "/sbin"
+              "/usr/sbin"
+              "/bin"
+              "/usr/bin"
+              "/opt/local/bin"
+              "/sw/bin"
+              "/usr/local/bin"
+              (expand-file-name "~/bin")
+              (expand-file-name "~/.emacs.d/bin")
+              ))
+ ;; PATH と exec-path に同じ物を追加します
+ (when (and (file-exists-p dir) (not (member dir exec-path)))
+   (setenv "PATH" (concat dir ":" (getenv "PATH")))
+   (setq exec-path (append (list dir) exec-path))))
+
+;; multi-term
+(require 'multi-term)
+(setq multi-term-program "/bin/bash")
 
 ;;============================================================================;;
 ;; auto-complete                                                              ;;
