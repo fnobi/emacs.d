3b9c07fee650fa7ad24503010689d24dae79cf50
fnobi <shin@fnobi.com>
2013-01-08 00:51:57 +0900

    anythingもいよいよ導入。

--- init.el
+++ init.el
@@ -134,9 +134,17 @@
 (auto-install-compatibility-setup)             ; 互換性確保
 
 ;;============================================================================;;
+;; anything                                                                   ;;
+;;============================================================================;;
+(require 'anything)
+(require 'anything-config)
+(add-to-list 'anything-sources 'anything-c-source-emacs-commands)
+
+(define-key global-map (kbd "C-;") 'anything)
+
+;;============================================================================;;
 ;; modules                                                                    ;;
 ;;============================================================================;;
-
 ;; flymake
 (defun next-flymake-error ()
   (interactive)
