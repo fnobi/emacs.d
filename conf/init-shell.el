fa1b990e9ced21519d9fbd6e0c78fd6b5e1f8b77
fnobi <shin@fnobi.com>
2013-01-23 03:00:42 +0900

    shellの設定など更新。*scratch*を消さない設定も入れた。

--- conf/init-shell.el
+++ conf/init-shell.el
@@ -3,35 +3,6 @@
 (dolist (path (reverse (split-string (getenv "PATH") ":")))
   (add-to-list 'exec-path path))
 
-;; ;; より下に記述した物が PATH の先頭に追加されます
-;; (dolist (dir (list
-;;       "/usr/local/mysql/bin"
-;;       "/opt/local/bin"
-;;       (expand-file-name "~/bin")
-;;       (expand-file-name "~/.emacs.d/bin")
-;;       "/usr/local/go/bin"
-;;       "/usr/X11/bin"
-;;       "/usr/local/bin"
-;;       "/sbin"
-;;       "/usr/sbin"
-;;       "/bin"
-;;       "/usr/bin"
-;;       "/usr/local/mysql/bin"
-;;       "/opt/local/sbin"
-;;       "/opt/local/bin"
-;;       "/usr/local/heroku/bin"
-;;       (expand-file-name "~/bin")
-;;       (expand-file-name "~/.rvm/bin")
-;;       (expand-file-name "~/.nodebrew/current/bin")
-;;       (expand-file-name "~/perl5/perlbrew/bin")
-;;       "/opt/local/sbin"
-;;       "/opt/local/bin"
-;;       ))
-;;  ;; PATH と exec-path に同じ物を追加します
-;;  (when (and (file-exists-p dir) (not (member dir exec-path)))
-;;    (setenv "PATH" (concat dir ":" (getenv "PATH")))
-;;    (setq exec-path (append (list dir) exec-path))))
-
 ;; multi-term
 (require 'multi-term)
 (setq multi-term-program "/bin/bash")
@@ -43,3 +14,5 @@
      (define-key term-raw-map (kbd "C-y") 'term-paste)
      ))
 (define-key global-map (kbd "C-x t") 'multi-term)
+
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-off)
